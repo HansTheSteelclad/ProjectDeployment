3655730ab493883317f678b2d5527e4f59505dd2
HansTheSteelclad <149094586+HansTheSteelclad@users.noreply.github.com>
2025-01-13 19:19:31 +0100

    Add files via upload

--- Deployment_version/urls.py
+++ Deployment_version/urls.py
@@ -3,16 +3,16 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("Oferty_pracy/", views.oferty_pracy_disp, name="Oferty_pracy"),
-    path("Analiza_ofert/", views.analiza_wykresy, name="Analiza"),
-    path("Analiza_pensji/", views.analiza_pensje, name="Analiza_pensje"),
-    path("Analiza_ilosci_ofert/", views.analiza_ilosc, name="Analiza_ilosc_ofert"),
-    path("Analiza_opisowa/", views.analiza_opisowa, name="Analiza_oferty"),
-    path("Analiza_ilosci_ofert_w_czasie/", views.analiza_ilosc_2, name="Analiza_ilosc_ofert_2"),
-    path("Error/", views.analiza_wykresy, name="Error"),
-    path("Error/", views.analiza_pensje, name="Error"),
-    path("Error/", views.analiza_ilosc, name="Error"),
-    path("Error/", views.analiza_opisowa, name="Error"),
+    path("templates/Oferty_pracy/", views.oferty_pracy_disp, name="Oferty_pracy"),
+    path("templates/Analiiza_ofert/", views.analiza_wykresy, name="Analiza_ofert"),
+    path("templates/Analiza_pensji/", views.analiza_pensje, name="Analiza_pensje"),
+    path("templates/Analiza_ilosci_ofert/", views.analiza_ilosc, name="Analiza_ilosc_ofert"),
+    path("templates/Analiza_opisowa/", views.analiza_opisowa, name="Analiza_opisowa"),
+    path("templates/Analiza_ilosci_ofert_w_czasie/", views.analiza_ilosc_2, name="Analiza_ilosc_ofert_2"),
+    path("templates/Error/", views.analiza_wykresy, name="Error"),
+    path("templates/Error/", views.analiza_pensje, name="Error"),
+    path("templates/Error/", views.analiza_ilosc, name="Error"),
+    path("templates/Error/", views.analiza_opisowa, name="Error")
 ]
 
 
